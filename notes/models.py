0d1c080f94951cd0f7617059dc82e3b61123dc6f
PlugaruT <plugaru.tudor@protonmail.com>
2021-08-06 22:27:37 +0300

    Remove models package

--- notes/models.py
+++ notes/models.py
@@ -1,5 +1,22 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
+# User model
 class GGITUser(AbstractUser):
     settings = models.TextField(blank=True, null=True)
+
+
+
+
+
+
+# Note model
+
+
+
+
+
+
+
+# NoteElement model
