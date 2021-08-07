31a6b700ef5269706a789f32d7492c7e3cc45d50
lya34 <istecalovici@gmail.com>
2021-08-07 16:48:25 +0300

    created models note & note element

--- notes/models.py
+++ notes/models.py
@@ -1,5 +1,9 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from datetime import datetime
+from django.db.models.fields.files import ImageFieldFile
+
+from django.db.models.fields.related import ForeignKey
 
 
 # User model
@@ -8,15 +12,31 @@ class GGITUser(AbstractUser):
 
 
 
-
-
-
 # Note model
+class Note(models.Model):
+     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+     title = models.CharField(max_length=200)
+     created_date = models.DateTimeField(
+            default=datetime.now)
+     updated_date = models.DateTimeField(
+            blank=True, null=True)
 
-
-
-
-
+     def updated_date(self):
+        self.updated_date = datetime.now()
+        self.save()
+     
+     def __str__(self):
+        return "Title:"+ self.title +"\nAuthor:"+self.author
 
 
 # NoteElement model
+class NoteElement(models.Model):
+    text = models.TextField(blank=True,null=True)
+    ##author=models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    note=ForeignKey(Note,on_delete=models.CASCADE)
+    image=models.TextField(blank=True,null=True)
+    video=models.TextField(blank=True,null=True)
+   
+    def __str__(self):
+        return self.text + self.image +self.video 
+   
