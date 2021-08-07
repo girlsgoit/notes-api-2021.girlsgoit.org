0b4ec963f275700ba4ac35fac28213d5d2645d04
Alexandrina <bagrin.alexandrina@gmail.com>
2021-08-07 22:30:31 +0300

    changed note and note element models

--- notes/models.py
+++ notes/models.py
@@ -1,9 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from datetime import datetime
-from django.db.models.fields.files import ImageFieldFile
 
-from django.db.models.fields.related import ForeignKey
 
 
 # User model
@@ -14,29 +11,18 @@ class GGITUser(AbstractUser):
 
 # Note model
 class Note(models.Model):
-     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-     title = models.CharField(max_length=200)
-     created_date = models.DateTimeField(
-            default=datetime.now)
-     updated_date = models.DateTimeField(
-            blank=True, null=True)
-
-     def updated_date(self):
-        self.updated_date = datetime.now()
-        self.save()
-     
-     def __str__(self):
-        return "Title:"+ self.title +"\nAuthor:"+self.author
-
-
-# NoteElement model
-class NoteElement(models.Model):
-    text = models.TextField(blank=True,null=True)
-    ##author=models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    note=ForeignKey(Note,on_delete=models.CASCADE)
-    image=models.TextField(blank=True,null=True)
-    video=models.TextField(blank=True,null=True)
-   
+    user = models.ForeignKey(GGITUser, on_delete = models.CASCADE, related_name="notes")
+    created_at = models.DateField(auto_now=True)
+    
     def __str__(self):
-        return self.text + self.image +self.video 
+        return f"{self.id}. Utilizator: {self.user}; Data creării: {self.created_at};"
+
+
+class NoteElement(models.Model):
+    tag = models.TextField()
+    content = models.TextField()
+    note = models.ForeignKey(Note, on_delete = models.CASCADE, related_name="note_elements")
+
+    def __str__(self):
+        return f"{self.id}. Tag: {self.tag}; {self.content}; {self.note}"
    
