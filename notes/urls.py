9b551755cf2c4d39cd2a04680a5536fcc0fcd6f6
aggresive-nicoleta <nicoletapetrea2002222@gmail.com>
2021-08-08 13:03:53 +0300

    user/user_id/ GET method created

--- notes/urls.py
+++ notes/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from notes.views import user_views
-from ..views import user_me
+from ..views import user_me, user_detail, register
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('user/register/', user_views.register, name='register'),
-
+    path('user/register/', register, name='register'),
+    path('user/<int:user_id>/', user_detail, name='user detail'),
 
     path('user/me', user_me, name='user_me'),
 ]
--- notes/views/user_views.py
+++ notes/views/user_views.py
@@ -3,6 +3,8 @@ from rest_framework.decorators import api_view, permission_classes
 from ..user_serializer import UserSerializer
 from rest_framework.response import Response
 from notes.serializers import user_serializer
+from django.shortcuts import get_object_or_404
+
 
 
 @api_view(["POST"])
@@ -23,5 +25,11 @@ def user_me(request):
     serialised_user_me = UserSerializer(user_data)
     return Response(serialised_user_me.data)
 
+@api_view(["GET"])
+@permission_classes(['IsAuthenticated'])
+def user_detail(request,user_id):
+   user = get_object_or_404(GGITUser, pk=user_id)
+   if request.method == "GET":
+      serialized_user = UserSerializer(user)
+      return Response(serialized_user.data)
 
-p = 5
