dc0341db307cb8adb41ab73e9c564ffab01821c4
Marc Graciov <mark.graciov@gmail.com>
2021-08-08 15:17:22 +0100

    add swagger docs?

--- notes_api/urls.py
+++ notes_api/urls.py
@@ -16,9 +16,23 @@ Including another URLconf
 from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Notes API",
+        default_version='v1',
+        description="Notes API for GirlsGoIT Summercamp",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('', include('notes.urls')),
 
 ]
