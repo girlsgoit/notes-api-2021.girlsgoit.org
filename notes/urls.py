e9b054a62db5913b585837c11eb5f25a596e0216
neliken <83429979+neliken@users.noreply.github.com>
2021-08-08 12:59:41 +0300

    is_unique_ user POST

--- notes/urls.py
+++ notes/urls.py
@@ -3,4 +3,5 @@ from notes.views import user_views
 
 urlpatterns = [
     path('user/register/', user_views.register, name='register'),
+    path('user/is-unique/', user_views.is_unique, name='is_unique'),
 ]
--- notes/views/user_views.py
+++ notes/views/user_views.py
@@ -1,15 +1,24 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from notes.serializers import user_serializer
+from notes.models import GGITUser
 
 
 @api_view(["POST"])
-
-# Create your views here.
 def register(request):
     if request.method == "POST":
-       print(request.data)
-       serializer = user_serializer.UserSerializer(data = request.data)
-       if serializer.is_valid():
-          serializer.save()
-       return Response()
+        print(request.data)
+        serializer = user_serializer.UserSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response()
+
+
+@api_view(["POST"])
+def is_unique(request):
+    if request.method == "POST":
+        print(request.data)
+        if GGITUser.objects.filter(username=request.data['username']).exists():
+            return Response(status = 400)
+        else:  
+            return Response(status = 200)
