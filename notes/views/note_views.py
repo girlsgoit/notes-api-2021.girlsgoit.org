5f9305d329466c107a9f8bdaf4ceb40aba3ba274
EliriaT <irina.tiora@isa.utm.md>
2021-08-08 16:24:58 +0300

    endpoint /notes/ updated with GET

--- notes/views/note_views.py
+++ notes/views/note_views.py
@@ -1,11 +1,19 @@
 from rest_framework.decorators import api_view
 from notes.serializers import note_serializer
 from rest_framework.response import Response
+from notes.models import Note
 
 
-@api_view(['POST'])
-def post_note(request):
-    serializer = note_serializer.NoteSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-    return Response()
+@api_view(['POST', 'GET'])
+def notes(request):
+    if request.method == 'POST':
+        new_data = request.data
+        new_data['user'] = request.user.id
+        serializer = note_serializer.NoteSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response()
+    elif request.method == 'GET':
+        note = Note.objects.all()
+        serializer = note_serializer.NoteSerializer(note, many=True)
+        return Response(serializer.data)
