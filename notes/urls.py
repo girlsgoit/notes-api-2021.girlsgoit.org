3198d0ccd4898fbecc774a8ed61645d8f1059a32
PlugaruT <plugaru.tudor@protonmail.com>
2021-08-13 18:47:12 +0300

    2 more

--- notes/urls.py
+++ notes/urls.py
@@ -3,8 +3,8 @@ from notes.views import user_views, note_views
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('user/register/', user_views.register, name='register'),
-    path('user/me', user_views.user_me, name='user_me'),
+    path('users/register/', user_views.register, name='register'),
+    path('users/me', user_views.user_me, name='user_me'),
     path('notes', note_views.notes, name='notes'),
     path('users', user_views.all_users, name='all_users'),
     path('users/login', obtain_auth_token, name='login'),
