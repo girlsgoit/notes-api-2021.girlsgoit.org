1bc62453356e0aaae39f213f7b302ec0a20c21fd
Marc Graciov <mark.graciov@gmail.com>
2021-08-14 06:38:36 +0100

    Add example swagger_auto_schema

--- notes/views/note_views.py
+++ notes/views/note_views.py
@@ -1,9 +1,19 @@
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import status
 from rest_framework.decorators import api_view
 from notes.serializers import note_serializer
 from rest_framework.response import Response
 from notes.models import Note
 
 
+@swagger_auto_schema(
+    method='get', operation_description="Get the list of notes.",
+    responses={200: note_serializer.NoteSerializer()},
+)
+@swagger_auto_schema(
+    method='post', operation_description="Create a new note.",
+    request_body=note_serializer.NoteSerializer(), responses={201: note_serializer.NoteSerializer()},
+)
 @api_view(['POST', 'GET'])
 def notes(request):
     if request.method == 'POST':
@@ -12,7 +22,7 @@ def notes(request):
         serializer = note_serializer.NoteSerializer(data=new_data)
         if serializer.is_valid():
             serializer.save()
-        return Response()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
     elif request.method == 'GET':
         note = Note.objects.all()
         serializer = note_serializer.NoteSerializer(note, many=True)
